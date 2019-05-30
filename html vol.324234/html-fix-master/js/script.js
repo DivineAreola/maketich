2238d8e43a29ca2e3196f2506050cd87d4260042
DivineAreola <51199125+DivineAreola@users.noreply.github.com>
2019-05-30 23:46:00 +0700

    Add files via upload

---
+++ html vol.324234/html-fix-master/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+  	{
+  		items: 1,
+  		center: true,
+  		nav: true,
+      navText : ["",""],
+  		dots: false,
+  		mouseDrag: false,
+  		touchDrag: false,
+  		pullDrag: false,
+  		//autoplay: true,
+  	});
+});
